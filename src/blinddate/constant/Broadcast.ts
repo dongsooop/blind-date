dd80d4ebe4698275757c3aca55681bf8c2e042d8
rdyjun <rdyjun00@gmail.com>
2025-10-31 09:21:22 +0900

    feat: broadcast event time UTC to KST

--- src/blinddate/constant/Broadcast.ts
+++ src/blinddate/constant/Broadcast.ts
@@ -2,12 +2,14 @@ export class Broadcast {
   public message: string;
   public memberId: number;
   public name: string;
-  public sendAt: Date;
+  public sendAt: string;
 
   constructor(message: string, memberId: number, name: string, sendAt: Date) {
     this.message = message;
     this.memberId = memberId;
     this.name = name;
-    this.sendAt = sendAt;
+    this.sendAt = sendAt.toLocaleDateString('ko-KR', {
+      timeZone: 'Asia/Seoul',
+    });
   }
 }
