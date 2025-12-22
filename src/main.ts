770bf564b4fb661e0dbba636def01f5deeb6c0f8
rdyjun <rdyjun00@gmail.com>
2025-12-22 16:16:12 +0900

    feat: add timestamp by logger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from '@/exception-filter/global.exception.filter';
 
+function patchConsole() {
+  const withTimestamp =
+    (method: (...args: any[]) => void) =>
+    (...args: any[]) => {
+      const now = new Date().toISOString();
+      method(`[${now}]`, ...args);
+    };
+
+  console.log = withTimestamp(console.log);
+  console.info = withTimestamp(console.info);
+  console.warn = withTimestamp(console.warn);
+  console.error = withTimestamp(console.error);
+}
+
+patchConsole();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
