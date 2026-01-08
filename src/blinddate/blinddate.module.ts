400a689198994a2b219b5acd5e34ed5f15fd75aa
rdyjun <rdyjun00@gmail.com>
2026-01-08 14:27:19 +0900

    feat(BlindDateController): added a request to the main server to send the notification

--- src/blinddate/blinddate.module.ts
+++ src/blinddate/blinddate.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BlindDateGateway } from '@/blinddate/gateway/blinddate.gateway';
 import { BlindDateMessage } from '@/blinddate/message/BlindDateMessage';
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { BlindDateService } from '@/blinddate/service/blinddate.service';
 import { BlindDateController } from '@/blinddate/controller/blinddate.controller';
 import { SessionModule } from '@/session/session.module';
 import { BlindDateRepository } from '@/blinddate/repository/blinddate.repository';
 
 @Module({
-  imports: [HttpModule, SessionModule],
+  imports: [HttpModule, SessionModule, HttpService],
   controllers: [BlindDateController],
   providers: [
     BlindDateGateway,
--- src/blinddate/controller/blinddate.controller.ts
+++ src/blinddate/controller/blinddate.controller.ts
@@ -8,10 +8,15 @@ import {
 } from '@nestjs/common';
 import { BlindDateService } from '@/blinddate/service/blinddate.service';
 import { BlindDateAvailableRequest } from '@/blinddate/dto/blinddate.available.dto';
+import { firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
 
 @Controller('blinddate')
 export class BlindDateController {
-  constructor(readonly blindDateService: BlindDateService) {}
+  constructor(
+    readonly blindDateService: BlindDateService,
+    readonly httpService: HttpService,
+  ) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
@@ -34,5 +39,32 @@ export class BlindDateController {
       request.maxSessionMemberCount,
     );
     await this.blindDateService.availableBlindDate(req);
+
+    const servers = process.env.MAIN_SERVER_CONTAINER_NAME?.split(',');
+    if (!servers) {
+      console.error('No server configured for notification');
+      return;
+    }
+
+    for (const server of servers) {
+      // 같은 네트워크 내 스프링 서비스로 POST 요청 (예: Docker Compose 서비스 이름 사용)
+      try {
+        const payload = {
+          expired: expired.toISOString(),
+          maxSessionMemberCount: request.maxSessionMemberCount,
+        };
+        await firstValueFrom(
+          this.httpService.post(
+            `http://${server}:8080/api/blinddate/notification`,
+            payload,
+          ),
+        );
+
+        console.log(`Notification sent to server: ${server}`);
+      } catch (e) {
+        // 실패 시 처리(로깅 등) 필요 시 추가
+        console.error('failed to send request for notification', e);
+      }
+    }
   }
 }
