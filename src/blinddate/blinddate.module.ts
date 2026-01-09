4e7e51c8a67ee6cb6f2704b126045f8acc179974
rdyjun <rdyjun00@gmail.com>
2026-01-09 17:50:17 +0900

    feat(BlindDateModule): remove HttpService

--- src/blinddate/blinddate.module.ts
+++ src/blinddate/blinddate.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BlindDateMessage } from '@/blinddate/message/BlindDateMessage';
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 import { BlindDateService } from '@/blinddate/service/blinddate.service';
 import { BlindDateController } from '@/blinddate/controller/blinddate.controller';
 import { SessionModule } from '@/session/session.module';
 import { BlindDateRepository } from '@/blinddate/repository/blinddate.repository';
 
 @Module({
-  imports: [HttpModule, SessionModule, HttpService],
+  imports: [HttpModule, SessionModule],
   controllers: [BlindDateController],
   providers: [BlindDateMessage, BlindDateService, BlindDateRepository],
   exports: [BlindDateService, BlindDateMessage],
